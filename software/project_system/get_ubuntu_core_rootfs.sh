a2e09c09678af4cb609d6833e4f8d0cfb42a860c
ImpactVPS-server <secretan@163.com>
2016-09-05 01:27:02 -0400

    增加文件系统ubunutu-core的下载,未测试是否能够使用

---
+++ software/project_system/get_ubuntu_core_rootfs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget http://cdimage.ubuntu.com/ubuntu-core/vivid/daily-preinstalled/current/vivid-preinstalled-core-armhf.tar.gz
+wget http://cdimage.ubuntu.com/ubuntu-core/vivid/daily-preinstalled/current/vivid-preinstalled-core-armhf.device.tar.gz
